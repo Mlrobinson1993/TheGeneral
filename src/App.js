77c70ed37c2216d9d7908f7a602465461fe2f88c
Mlrobinson1993 <38713556+Mlrobinson1993@users.noreply.github.com>
2019-09-26 12:06:15 -0300

    Updated about me text
    Updated about me text

--- src/App.js
+++ src/App.js
@@ -130,17 +130,7 @@ class App extends Component {
                       fontWeight: "300"
                     }}
                   >
-                    It is a long established fact that a reader will be
-                    distracted by the readable content of a page when looking at
-                    its layout. The point of using Lorem Ipsum is that it has a
-                    more-or-less normal distribution of letters, as opposed to
-                    using 'Content here, content here', making it look like
-                    readable English. Many desktop publishing packages and web
-                    page editors now use Lorem Ipsum as their default model
-                    text, and a search for 'lorem ipsum' will uncover many web
-                    sites still in their infancy. Various versions have evolved
-                    over the years, sometimes by accident, sometimes on purpose
-                    (injected humour and the like).
+                   We pride ourselves on a seasonal, locally sourced, innovative menu. Whether you're on a health kick or a carb binge, we have it all covered.
                   </Typography>
                 </Grid>
               </Grid>
